3f3eb341c9d120ed9a8bf64d4922ca10a10e67c5
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2020-04-23 10:09:08 +0530

    responsive nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  //code related to your props goes here
 }
+
+interface AppState {
+  sideDrawerOpen: any
+}
+
+class App extends React.Component<AppProps, AppState> {
+  
+  constructor(props: any) {
+    super(props)
+  
+    this.state = {
+      sideDrawerOpen: false
+    }
+  }
+  
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {
+        sideDrawerOpen: !prevState.sideDrawerOpen
+      }
+    })
+  }
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false})
+  }
+
+  render() {
+
+    let backdrop;
+
+    if(this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+
+    return (
+      <div style={{height: '100%'}}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+        <SideDrawer show={this.state.sideDrawerOpen}/>
+        {backdrop}
+        <main style={{marginTop: '64px'}}>
+          <p>this is page content!</p>
+        </main>
+      
+      </div>
+    )
+  }
+}
+
+// function App() {
+
+//   function drawerToggleClickHandlerr() {
+
+//   }
+
+//   return (
+//     <div style={{height: '100%'}}>
+//       <Toolbar/>
+//       <SideDrawer/>
+//       <Backdrop />
+//       <main style={{marginTop: '64px'}}>
+//         <p>this is page content!</p>
+//       </main>
+      
+//     </div>
+//   );
+// }
 
 export default App;
